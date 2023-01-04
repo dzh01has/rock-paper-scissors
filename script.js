6f1b47957cf4e3e6334a525da512999e292efb08
heXdzh <90160050+heXdzh@users.noreply.github.com>
2023-01-04 19:36:45 +0100

    Reworked the whole code to add a proper UI, still WIP

--- script.js
+++ script.js
@@ -1,78 +1,101 @@
 /** Array of possible selections for computer */
-
 const rndmSelection = ["rock", "paper", "scissors"];
-const possibleSelectionsPlayer = ["rock", "paper", "scissors"];
-let round = 0;
-let playerWins = 0;
-let computerWins = 0;
+var playerSelection = "";
+var computerSelection = "";
+var round = 0;
+var playerWins = 0;
+var computerWins = 0;
+
+const btn1 = document.querySelector('#Rock');
+const btn2 = document.querySelector('#Paper');
+const btn3 = document.querySelector('#Scissors');
+
+
+/** container and div for score and result */
+const container = document.querySelector('#container');
+
+const result = document.createElement('div');
+result.classList.add('result');
+container.appendChild(result);
+result.textContent = "Winner: "
+const rounds = document.createElement('div');
+rounds.classList.add('rounds');
+rounds.textContent = "Round: " + round;
+container.appendChild(rounds);
 
     function playRound(playerSelection, computerSelection)
     {
-
         /** All possible win conditions, for both the player and the computer */
-       if (possibleSelectionsPlayer.indexOf(playerSelection) === -1)
+       if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")
         {
            return "Wrong input!" 
         }  
        else if (playerSelection == "rock" && computerSelection == "paper")
         {
             computerWins++
-             return "Computer wins! You have " + playerSelection + " and the PC has " + computerSelection
+            result.textContent = "Winner: Computer wins! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;
         }
 
         else if (playerSelection == "rock" && computerSelection == "scissors")
         {
             playerWins++
-            return "You win! You have " + playerSelection + " and the PC has " + computerSelection
+            result.textContent = "Winner: You win! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;
         }
 
         else if (playerSelection == "paper" && computerSelection == "rock")
         {
             playerWins++
-            return "You win! You have " + playerSelection + " and the PC has " + computerSelection
+            result.textContent = "Winner: You win! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;
         }
 
         else if (playerSelection == "paper" && computerSelection == "scissors")
         {
             computerWins++
-            return "Computer wins! You have " + playerSelection + " and the PC has " + computerSelection
+            result.textContent = "Winner: Computer wins! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;
         }
         else if (playerSelection == "scissors" && computerSelection == "paper")
         {
             playerWins++
-            return "You win! You have " + playerSelection + " and the PC has " + computerSelection
+            result.textContent = "Winner: You win! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;
         }
 
         else if (playerSelection == "scissors" && computerSelection == "rock")
         {
             computerWins++
-            return "Computer wins! You have " + playerSelection + " and the PC has " + computerSelection
+            result.textContent = "Winner: Computer wins! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;
         }
 
-            /** Draw conditions */
-
+        /** Draw conditions */
         else 
         {
             switch (true) 
             {
             case (playerSelection && computerSelection == "rock"):
 
-                    return "Draw! You have " + playerSelection + " and the PC has " + computerSelection
+            result.textContent = "Winner: No one, it's a draw! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;
        
             case (playerSelection && computerSelection == "paper"):
 
-                    return "Draw! You have " + playerSelection + " and the PC has " + computerSelection   
+            result.textContent = "Winner: No one, it's a draw! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;  
 
             case (playerSelection && computerSelection == "scissors"):
 
-                    return "Draw! You have " + playerSelection + " and the PC has " + computerSelection   
+            result.textContent = "Winner: No one, it's a draw! You have " + playerSelection + " and the PC has " + computerSelection
+            rounds.textContent = "Round: " + round;  
           
             default:
                     return "Poop"
             }
  
-        }
-         
+        }  
     }; 
     
     /** Random choice from the array we created at the start */
@@ -82,42 +105,96 @@ function getComputerChoice()
     return rndmSelection[~~(Math.random() * rndmSelection.length)];
 }
 
-    /** Prompt to get input from the player */
-
-function getPlayerChoice()
-{
-    return prompt("Please choose between 'rock', 'paper' and 'scissors' and write it here");
-}
-
-    /** Uses the function under this one in a 'for loop' to end the game after 5 rounds, keeps score to decide the ultimate winner */
-
-function startGame()
-{
-    for (let i = 0; i <=5; i++) 
+   if (round == 0)
     {
-        round++
-        console.log(game(playRound));
+        btn1.addEventListener('click', function (e) 
+        {
+            round ++;
+            playerSelection = "rock"
+            computerSelection = getComputerChoice();
+            if (round == 5 && playerWins > computerWins)
+            {
+                result.textContent = "You win the game!!!!!"
+                rounds.textContent = "Round: 5";
+                btn1.disabled = true;
+                btn2.disabled = true;
+                btn3.disabled = true;
+                return
+            }
+            else if (round == 5 && playerWins < computerWins)
+            {
+                result.textContent = "Computer wins the game! World domination by AI is imminent!"
+                rounds.textContent = "Round: 5";
+                btn1.disabled = true;
+                btn2.disabled = true;
+                btn3.disabled = true;
+                return
+            }
+            else
+            {
+                console.log(playRound(playerSelection, computerSelection));
+            }  
+        });
 
-        if (round == 5 && playerWins > computerWins)
+        btn2.addEventListener('click', function (e) 
         {
-            round = 0;
-            return "You win the game!"
-        }
-        else if (round == 5 && playerWins < computerWins)
+            round ++;
+            playerSelection = "paper"
+            computerSelection = getComputerChoice();
+            if (round == 5 && playerWins > computerWins)
+            {
+                result.textContent = "You win the game!!!!!"
+                rounds.textContent = "Round: 5";
+                btn1.disabled = true;
+                btn2.disabled = true;
+                btn3.disabled = true;
+                return
+            }
+            else if (round == 5 && playerWins < computerWins)
+            {
+                result.textContent = "Computer wins the game! World domination by AI is imminent!"
+                rounds.textContent = "Round: 5";
+                btn1.disabled = true;
+                btn2.disabled = true;
+                btn3.disabled = true;
+                return
+            }
+            else
+            {
+                console.log(playRound(playerSelection, computerSelection));
+            }    
+        });
+
+        btn3.addEventListener('click', function (e) 
         {
-            round = 0;
-            return "Computer wins the game! World domination by AI is imminent!"
-        }    
+            round ++;
+            playerSelection = "scissors"
+            computerSelection = getComputerChoice();
+            if (round == 5 && playerWins > computerWins)
+            {
+                result.textContent = "You win the game!!!!!";
+                rounds.textContent = "Round: 5";
+                btn1.disabled = true;
+                btn2.disabled = true;
+                btn3.disabled = true;
+                return
+            }
+            else if (round == 5 && playerWins < computerWins)
+            {
+                result.textContent = "Computer wins the game! World domination by AI is imminent!";
+                rounds.textContent = "Round: 5";
+                btn1.disabled = true;
+                btn2.disabled = true;
+                btn3.disabled = true;
+                return
+            }
+            else
+            {
+                console.log(playRound(playerSelection, computerSelection));
+            }  
+        });
     } 
-}
 
- /** Function to start the game */
+    
 
-function game(playRound)
-{   
-    let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerChoice();
-    return playRound(playerSelection, computerSelection);    
-}
 
-console.log(startGame(game))
